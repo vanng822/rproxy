a4943c7470f5494a3cce23ca0cb2836f3fefa3aa
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-01 21:57:27 +0200

    Simpler version

--- proxy.go
+++ proxy.go
@@ -21,28 +21,19 @@ func main() {
 	flag.StringVar(&proxy, "r", "", "Proxy host")
 	flag.Parse()
 
-	proxyUrl, err := url.Parse(proxy)
+	targetUrl, err := url.Parse(proxy)
 	
 	if err != nil {
 		log.Fatal(err)
 	}
 	
-	if proxyUrl.Host == "" {
+	if targetUrl.Host == "" {
 		log.Fatalf("You need to provide a valid proxy url. Provided '%s'", proxy)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		director := func(req *http.Request) {
-			req = r
-			if proxyUrl.Scheme == "" {
-				proxyUrl.Scheme = "http"
-			}
-			req.URL.Scheme = proxyUrl.Scheme
-			req.URL.Host = proxyUrl.Host
-		}
-		proxy := &httputil.ReverseProxy{Director: director}
-		proxy.ServeHTTP(w, r)
-	})
+	singleHostProxy := httputil.NewSingleHostReverseProxy(targetUrl)
+	
+	http.Handle("/", singleHostProxy)
 	
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil))
 }
