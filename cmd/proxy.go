006dd4ac272c6642835508aff5c8d1ac3afe3395
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-15 22:18:17 +0200

    A working proxy version with dynamic register of backend nodes

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -3,40 +3,69 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/vanng822/r2router"
+	"github.com/vanng822/rproxy"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"github.com/vanng822/rproxy"
 )
 
 func main() {
 	var (
-		host    string
-		port    int
-		proxy   string
+		host  string
+		port  int
 	)
 
 	flag.StringVar(&host, "h", "127.0.0.1", "Host to listen on")
 	flag.IntVar(&port, "p", 80, "Port number to listen on")
-	flag.StringVar(&proxy, "r", "", "Proxy host")
 	flag.Parse()
 
-	targetUrl, err := url.Parse(proxy)
-	
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	if targetUrl.Host == "" {
-		log.Fatalf("You need to provide a valid proxy url. Provided '%s'", proxy)
-	}
-	
-	rproxy.Server{}
-	
-	singleHostProxy := httputil.NewSingleHostReverseProxy(targetUrl)
-	
-	http.Handle("/", singleHostProxy)
-	
+	proxyServer := rproxy.NewProxy()
+
+	seefor := r2router.NewSeeforRouter()
+
+	seefor.Group("/_server", func(r *r2router.GroupRouter) {
+		r.Post("/add", func(w http.ResponseWriter, req *http.Request, _ r2router.Params) {
+			log.Println("_server/add")
+			err, serverName, targetUrl := proxyServer.ParseServerConfig(req)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Invalid server config, error: %s", err.Error()), http.StatusBadRequest)
+				return
+			}
+			err = proxyServer.Register(serverName, targetUrl)
+			if err != nil {
+				http.Error(w,
+					fmt.Sprintf("It was problem when adding new server, serverName: '%s', targetUrl: '%s', error: '%s'",
+						serverName, targetUrl, err.Error()),
+					http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK"))
+			
+		})
+
+		r.Delete("/remove", func(w http.ResponseWriter, req *http.Request, _ r2router.Params) {
+			log.Println("_server/remove")
+			err, serverName, targetUrl := proxyServer.ParseServerConfig(req)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("Invalid server config, error: %s", err.Error()), http.StatusBadRequest)
+				return
+			}
+			err = proxyServer.Unregister(serverName, targetUrl)
+			if err != nil {
+				http.Error(w,
+					fmt.Sprintf("It was problem when removing server, serverName: '%s', targetUrl: '%s', error: '%s'",
+						serverName, targetUrl, err.Error()),
+					http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK"))
+			
+		})
+	})
+
+	http.Handle("/", proxyServer)
+	go http.ListenAndServe(fmt.Sprintf("%s:%d", host, port + 1), seefor)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil))
 }
