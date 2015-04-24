bf4d2e16c48812f7fa5e7e95f9d44d4409e42436
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-24 19:12:40 +0200

    Adding more tests for backend

--- backend_test.go
+++ backend_test.go
@@ -12,3 +12,30 @@ func TestBackendAddNode(t *testing.T) {
 	b.addNode("http://127.0.0.1:8080")
 	assert.Len(b.nodes, 1)
 }
+
+func TestBackendDeleteNode(t *testing.T) {
+	assert := assert.New(t)
+	b := NewBackend()
+	b.addNode("http://127.0.0.1:8080")
+	assert.Len(b.nodes, 1)
+	b.deleteNode("http://127.0.0.1:8080")
+	assert.Len(b.nodes, 0)
+}
+
+func TestBackendNextNode(t *testing.T) {
+	assert := assert.New(t)
+	nodes := []string{"http://127.0.0.1:8080", "http://127.0.0.1:8081", "http://127.0.0.1:8082"}
+	b := NewBackend()
+	
+	for _, n := range nodes {
+		b.addNode(n)
+	}
+	assert.Len(b.nodes, 3)
+	
+	for i := 0; i < 3; i++ {
+		for _, n := range nodes {
+			node := b.nextNode()
+			assert.Equal(node.targetUrl, n)
+		}
+	}
+}
